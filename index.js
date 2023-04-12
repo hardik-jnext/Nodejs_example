5ea25f8eab39a44cdceabfc9e9ba7aa960036817
hardik <hardik.jnext@gmail.com>
2023-04-12 18:50:22 +0530

    swagger setup

--- index.js
+++ index.js
@@ -9,6 +9,28 @@ const path = require("path")
 const db = require("./src/config/Config.js")
 const body = require("body-parser")
 const  hadleErrorMessage  = require('./src/middleware/errorHandle.js')
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require("swagger-jsdoc")
+
+
+const options ={
+    definition :{
+        openapi : "3.0.0",
+        info:{
+            title : "Nodejs example",
+            version : "1.0.0",
+            description :"A complete nodejs example"
+        },
+        servers : [{
+            url : "http://localhost:3000"
+        }],    
+    },
+  apis :["src/routes/*.js"]
+}
+const specs = swaggerJsDoc(options)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+
+ 
 
 // Task no.:50 (Implement i18n for multi-language in response.)
     
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,58 +1,187 @@
 const express = require("express");
 const router = express.Router();
-const {getUser,allUser,getAllRegister,createUser,loginUser,updateUserWithoutAuth,updateUser,deleteUserWithoutAuth,deleteUserWithAuth,roleUser,orderrole,onlyadmin,deleteItem,sendmail,verifyOtp,changepassword,forgetPassword,forgetPasswordmail} = require("../controller/users.controllers.js");
+const {
+  getUser,
+  allUser,
+  getAllRegister,
+  createUser,
+  loginUser,
+  updateUserWithoutAuth,
+  updateUser,
+  deleteUserWithoutAuth,
+  deleteUserWithAuth,
+  roleUser,
+  orderrole,
+  onlyadmin,
+  deleteItem,
+  sendmail,
+  verifyOtp,
+  changepassword,
+  forgetPassword,
+  forgetPasswordmail,
+} = require("../controller/users.controllers.js");
 const authUser = require("../middleware/user.auth.js");
-const {celebrate} = require("celebrate")
-const {usercreatevalid,findUser,updateUservalidation,onlyadminValidation,changepasswordvalid,forgetPasswordvalid} = require("../validator/users.validator.js")
-    
-             //get all register user
-router.route("/all-registerUser").get(getAllRegister)
+const { celebrate } = require("celebrate");
+const {
+  usercreatevalid,
+  findUser,
+  updateUservalidation,
+  onlyadminValidation,
+  changepasswordvalid,
+  forgetPasswordvalid,
+} = require("../validator/users.validator.js");
 
-          // get all user with auth 
+
+
+
+/**
+ *@swagger
+ * componenets :
+ *  schemas:
+ *    user:
+ *        type: object
+ *        properties:
+ *         id:
+ *            type: integer
+ *         userName: 
+ *            type: string
+ *         email:
+ *            type: string
+ *         age :
+ *            type: integer 
+ *         address:
+ *            type: string
+ *         role:
+ *            type: string
+ *         status:
+ *            type: string
+ *         isVerify:
+ *            type: boolean
+ *         otp:
+ *            type: integer
+ *         expireOtpTime:
+ *            type: string
+ *            format: date
+ *         password:
+ *            type: string
+ *  
+ *
+ * 
+ */         
+
+//get all register user
+
+/**
+ * @swagger
+ * /all-registerUser:
+ *     get:
+ *       summary: get all users
+ *       responses:
+ *          200:
+ *            description : list of register user
+ *            content:
+ *              application/json:
+ *                schema:
+ *                   type: array
+ *      
+ */             
+router.route("/all-registerUser").get(getAllRegister);
+
+// get all user with auth
+/**
+ * @swagger
+ * /alluser-withAuth:
+ *     get:
+ *       summary: get all users
+ *       responses:
+ *          200:
+ *            description : list of register user
+ *            content:
+ *              application/json:
+ *                schema:
+ *                   type: array
+ *      
+ */             
+
 router.route("/alluser-withAuth").get(authUser, allUser);
+
+/**
+ * @swagger
+ * /login:
+ *     post:
+ *       summary: get all users
+ *       consumes:
+*          - json/plain  
+ *       parameters:
+*        - in: body
+*          email: user
+*          description: Find Token
+*          schema:
+*            type: object
+*            required:
+*              - email
+*
+ *       responses:
+ *          200:
+ *            description : list of register user
+ *            content:
+ *              application/json:
+ *                schema:
+ *                   type: array
+ *      
+ */             
 
 router.post("/login", loginUser);
 
-router.route("/getUser/:id").get(celebrate(findUser),getUser);
-      
+router.route("/getUser/:id").get(celebrate(findUser), getUser);
+
 //user register api
-router.post("/register",celebrate(usercreatevalid),createUser);
+router.post("/register", celebrate(usercreatevalid), createUser);
 
+// without auth
+router.put("/update-User-WithoutAuth/:id", updateUserWithoutAuth);
+//with auth
+router.put(
+  "/update-User-WithAuth",
+  celebrate(updateUservalidation),
+  authUser,
+  updateUser
+);
 
-        // without auth
-router.put("/update-User-WithoutAuth/:id",updateUserWithoutAuth)
-      //with auth
-router.put("/update-User-WithAuth",celebrate(updateUservalidation),authUser,updateUser)
+// without auth
+router.delete("/delete-without-auth/:id", deleteUserWithoutAuth);
+// without auth
+router.delete("/delete-with-auth", authUser, deleteUserWithAuth);
 
+router.get("/userRole", authUser, roleUser);
 
+router.get("/orderrole", authUser, orderrole);
 
-    // without auth
-router.delete("/delete-without-auth/:id",deleteUserWithoutAuth)
-     // without auth
-router.delete("/delete-with-auth",authUser,deleteUserWithAuth)
+//only admin
+router.get("/onlyadmin", celebrate(onlyadminValidation), authUser, onlyadmin);
 
-
-router.get("/userRole",authUser,roleUser)
-
-router.get("/orderrole",authUser,orderrole)
-  
-  //only admin 
-router.get("/onlyadmin",celebrate(onlyadminValidation),authUser,onlyadmin)
-
-router.delete('/deleteitem/:id',authUser,deleteItem)
+router.delete("/deleteitem/:id", authUser, deleteItem);
 
 //email
-router.get("/gmail",sendmail)
+router.get("/gmail", sendmail);
 
 //verify otp
- router.post("/verifyOtp/:otp",verifyOtp)
+router.post("/verifyOtp/:otp", verifyOtp);
 
 // change password
-router.put("/changepassword",celebrate(changepasswordvalid),authUser,changepassword)
-
+router.put(
+  "/changepassword",
+  celebrate(changepasswordvalid),
+  authUser,
+  changepassword
+);
 
 // forgot password
-router.get("/forgotpasswordmail/",forgetPasswordmail) 
-router.put("/forgotpassword/:otp",celebrate(forgetPasswordvalid),forgetPassword) 
-  
+router.get("/forgotpasswordmail/", forgetPasswordmail);
+router.put(
+  "/forgotpassword/:otp",
+  celebrate(forgetPasswordvalid),
+  forgetPassword
+);
+
 module.exports = router;
